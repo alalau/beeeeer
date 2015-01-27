024b5811a0aa48253618fc2252e1aaa3f91dd396
Aaron La Lau <aaron.lalau@gmail.com>
2015-01-27 14:46:51 -0800

    fab control

--- app/scripts/controllers/brewery.js
+++ app/scripts/controllers/brewery.js
@@ -10,10 +10,10 @@
 angular.module('beerApp')
   .controller('BreweryDetailsController', ['$scope', '$routeParams', '$http',
   function($scope, $routeParams, $http) {
-    $http.get('beers/beers.json').success(function(data) {
+    $http.get('json/breweries.json').success(function(data) {
       	angular.forEach(data, function(item) {
-          if (item.id == $routeParams.beerId) 
-            $scope.beer = item;
+          if (item.id == $routeParams.breweryId) 
+            $scope.brewery = item;
         });
     });
 }]);
