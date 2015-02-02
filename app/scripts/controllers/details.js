a72394b623ef2bcd094845caefdc46799ead2079
heyilovepie <gillianramsay@shaw.ca>
2015-02-01 21:58:13 -0800

    Fixing canvas' bad but working

--- app/scripts/controllers/details.js
+++ app/scripts/controllers/details.js
@@ -18,20 +18,38 @@ angular.module('beerApp')
 	        function sketchAlcohol(processing){
 	    	//still can't figure out why the canvas size won't change from  the  default :(
 		    	processing.setup = function(){
-		    		processing.size(500, 100);
+		    		processing.size(500, 500);
 		    	};
 				processing.draw = function(){
 					processing.size(processing.displayWidth, processing.displayHeight);
 					processing.background(0);
-					processing.fill(255, 0, 0);
+					processing.fill(100);
 					processing.noStroke();
-					processing.rect(processing.width/10, processing.height/10, 
-						processing.width*($scope.beer.alcohol)/100, processing.height/8);
+					processing.rect(0, 0,
+						processing.width*($scope.beer.alcohol)/100, processing.height);
 				};
 			}
 
-			var canvas = document.getElementById("canvasAlcohol");
-			var p = new Processing(canvas, sketchAlcohol);
+			function sketchBitterness(processing){
+	    	//still can't figure out why the canvas size won't change from  the  default :(
+		    	processing.setup = function(){
+		    		processing.size(500, 500);
+		    	};
+				processing.draw = function(){
+					processing.size(processing.displayWidth, processing.displayHeight);
+					processing.background(0);
+					processing.fill(100);
+					processing.noStroke();
+					processing.rect(0, 0, 
+						processing.width*($scope.beer.bitterness)/100, processing.height);
+				};
+			}
+
+			var canvasA = document.getElementById("canvasAlcohol");
+			var pA = new Processing(canvasA, sketchAlcohol);
+
+			var canvasB = document.getElementById("canvasBitterness");
+			var pB = new Processing(canvasB, sketchBitterness);
     	});
     });
     $scope.pageClass = 'page-details';
