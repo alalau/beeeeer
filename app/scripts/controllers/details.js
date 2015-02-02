cfacf43761591b40231e8d9132a9778d2838d3da
heyilovepie <gillianramsay@shaw.ca>
2015-02-01 22:38:59 -0800

    bar canvas much better now.

--- app/scripts/controllers/details.js
+++ app/scripts/controllers/details.js
@@ -16,28 +16,29 @@ angular.module('beerApp')
             	$scope.beer = item;
 
 	        function sketchAlcohol(processing){
-	    	//still can't figure out why the canvas size won't change from  the  default :(
 		    	processing.setup = function(){
-		    		processing.size(500, 500);
+		    		processing.size(300, 50);
+		    		processing.background(100);
+		    		processing.noStroke();
+		    		processing.frameRate(10);
 		    	};
 				processing.draw = function(){
-					processing.size(processing.displayWidth, processing.displayHeight);
-					processing.background(0);
-					processing.fill(100);
-					processing.noStroke();
-					processing.rect(0, 0,
-						processing.width*($scope.beer.alcohol)/100, processing.height);
+					//processing.colorMode(processing.HSB, 360, 100, 100, 100);
+					//processing.fill(processing.random(20, 35), 100, processing.random(30, 80), processing.random(30, 80));
+					//processing.ellipse(processing.random(0, processing.width*($scope.beer.alcohol)/100), processing.random(0, processing.height),
+					//	processing.random(5, 10), processing.random(5, 10));
+					processing.fill(255);
+					processing.rect(processing.width*($scope.beer.alcohol)/100, 0,
+						processing.width*(1-$scope.beer.alcohol/100), processing.height);
 				};
 			}
 
 			function sketchBitterness(processing){
-	    	//still can't figure out why the canvas size won't change from  the  default :(
 		    	processing.setup = function(){
 		    		processing.size(500, 500);
+		    		processing.background(255);
 		    	};
 				processing.draw = function(){
-					processing.size(processing.displayWidth, processing.displayHeight);
-					processing.background(0);
 					processing.fill(100);
 					processing.noStroke();
 					processing.rect(0, 0, 
